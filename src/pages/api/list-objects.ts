40c2f3dffbe3c94e3e35cfd312c01c7e0f26a756
Johnervan Lee <johnervan@gmail.com>
2022-11-28 23:42:07 +0800

    fix type error

--- src/pages/api/list-objects.ts
+++ src/pages/api/list-objects.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type {  NextApiRequest,  NextApiResponse } from 'next';
-import {  listObjects } from '../../services/aws/S3';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { listObjects } from '../../services/aws/S3';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   const { prefix } = req.query;
